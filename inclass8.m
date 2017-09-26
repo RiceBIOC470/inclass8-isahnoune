1f281c6aefd9e1903d63782865d98b8a4fb8ac5b
isahnoune <31284645+isahnoune@users.noreply.github.com>
2017-09-26 12:39:25 -0500

    Add files via upload

--- inclass8.m
+++ inclass8.m
@@ -2,11 +2,47 @@
 
 %Using the swalign function
 % A. Create a random DNA sequence of length 100. 
+
+% dnaletters = ['A', 'T', 'G', 'C']
+% randomDNAseq = dnaletters(randi(4, 1, 100))
+
+seq1 = randseq(100);
 % B. Create a second sequence from the first one by changing the middle 20 basepairs (at positions 41-60)
+
+seq2 = strcat(seq1(1:40), randseq(20), seq1(61:100));
+
 % C. run swalign on the two sequences with the default parameters
+
+sequencealignment = swalign(seq1, seq2)
+
+
 % D. run swalign with much higher and lower values of the GapOpen parameter
 % and explain the results. 
+
+sequencealignment2 = swalign(seq1, seq2, 'GapOpen', 100)
+sequencealignment3 = swalign(seq1, seq2, 'GapOpen', 1)
+
+%GapOpen signifies the penalty for opening a gap in the alignment. A higher
+%gap penalty is used to reduce gap openings in the sequence, whereas the
+%converse is used to increase gap openings. Therefore the relationship is
+%inverse. Higher gap penalty leads to lower value and vice versa.
+
+
 % E. run swalign with a low value of GapOpen but a high value of ExtendGap,
 % explain the result
+
+sequencealignment4 = swalign(seq1, seq2, 'GapOpen', 1, 'ExtendGap', 100)
+
+%Low value of GapOpen and high value of ExtendGap leads to a value very
+%similar to the result received in sequence alignment 3, which indicates
+%that the GapOpen value plays a larger value in how the penalties are
+%displayed and analyzed. 
+
 % F. run swalign align with the default value of GapOpen and a low value
 % of ExtendGap and explain the result. 
+
+sequencealignment5 = swalign(seq1, seq2, 'ExtendGap', 1)
+
+%A low value of ExtendGap yields to a lower penalty of extending the gap in
+%the sequence alignment, as compared to when GapOpen is higher, shown in
+%sequence alignment 4.

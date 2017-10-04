70fcdd6860ea00964f1b6f53c7fdedb02de5cc19
warmflasha <warmflasha@gmail.com>
2017-10-04 18:04:02 -0500

    graded inclass8

--- inclass8.m
+++ inclass8.m
@@ -1,3 +1,5 @@
+%AW: see comments below. 0.9/1.
+
 %Inclass assignment 8
 
 %Using the swalign function
@@ -19,6 +21,7 @@ sequencealignment = swalign(seq1, seq2)
 % D. run swalign with much higher and lower values of the GapOpen parameter
 % and explain the results. 
 
+%Note by default swalign uses the aminoacid alphabet. You should set 'alphabet' to 'nt' -0.05. 
 sequencealignment2 = swalign(seq1, seq2, 'GapOpen', 100)
 sequencealignment3 = swalign(seq1, seq2, 'GapOpen', 1)
 
@@ -26,6 +29,8 @@ sequencealignment3 = swalign(seq1, seq2, 'GapOpen', 1)
 %gap penalty is used to reduce gap openings in the sequence, whereas the
 %converse is used to increase gap openings. Therefore the relationship is
 %inverse. Higher gap penalty leads to lower value and vice versa.
+
+%AW: note also the inverse relationship between having gaps and having mismatches. -0.05.
 
 
 % E. run swalign with a low value of GapOpen but a high value of ExtendGap,
@@ -38,6 +43,9 @@ sequencealignment4 = swalign(seq1, seq2, 'GapOpen', 1, 'ExtendGap', 100)
 %that the GapOpen value plays a larger value in how the penalties are
 %displayed and analyzed. 
 
+%AW: note that gaps are generally shorter - it will not make gaps longer than
+% 1 bp due to the high penalty associated with this. -0.05
+
 % F. run swalign align with the default value of GapOpen and a low value
 % of ExtendGap and explain the result. 
 
@@ -46,3 +54,4 @@ sequencealignment5 = swalign(seq1, seq2, 'ExtendGap', 1)
 %A low value of ExtendGap yields to a lower penalty of extending the gap in
 %the sequence alignment, as compared to when GapOpen is higher, shown in
 %sequence alignment 4.
+%AW: so as a result, there are fewer, but longer, gaps. 
